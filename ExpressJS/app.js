b7e68fb3597c2b1f3439e0c179cd0c69b5e2112b
mahiboobnadaf <rasool97260@gmail.com>
2024-02-19 16:00:46 +0530

    example to notation

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+//Route to
+//http://localhost:3000/
 app.get('/',function(req,res){
     res.send("This is HomePage");
 });
 
+
+//http://localhost:3000/contact
 app.get('/contact',function(req,res){
     res.send("This is Contact Page")
 })
