829d3e5957055984feb3ccfa9d4a31ea0afe689b
mahiboobnadaf <rasool97260@gmail.com>
2024-02-19 17:10:52 +0530

    Template engine EJS example

---
+++ ExpressJS/Templete_engine/app.js
@@ -0,0 +1,17 @@
+// first install ejs with npm install ejs
+// Create a Views folder within that create a file which you want to render and that should be .ejs 
+// ex: html cintents can be copied in ejs file
+// the render method will defaulty search for views folder and the file which is given as first parameter to render()
+// the second param of render() is an object in which we store the name passed in url and that object can be used in ejs file to modify
+
+const express = require('express');
+
+const app = express();
+
+app.set('view engine','ejs');
+
+app.get('/profile/:name',function(req,res){
+    res.render('profile',{person:req.params.name}); 
+});
+
+app.listen(3000);
